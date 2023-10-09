45569f922f4c44eb46b045929d67f0646c3ffc3e
nazwakito <nazwaleony77@gmail.com>
2023-10-09 15:32:11 +0700

    route index

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ app.get("/", (req, res) => {
 	res.send("Hello World!");
 });
 
+//import route posts
+const postsRouter = require('./routes/posts');
+app.use('/api/posts', postsRouter); // use route posts di Express
+
 app.listen(port, () => {
-	console.log(`app running at http://localhost:${port}`);
-});
+  console.log(`app running at http://localhost:${port}`)
+})
+
