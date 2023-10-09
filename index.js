bb1946a1e5f17cc2963a42cb7deec68b1c852e63
cicicisilia <cicicisilia4@gmail.com>
2023-10-09 15:19:18 +0700

    install nodemon

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-	res.send("Hello World!");
-});
+
+//import route posts
+const postsRouter = require("./routes/postsRoutes");
+app.use("/api/posts", postsRouter); // use route posts di Express
 
 app.listen(port, () => {
 	console.log(`app running at http://localhost:${port}`);
