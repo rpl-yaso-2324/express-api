3e697f77d162d70ff22812ebd497c9ff7cb22a18
sopian132 <sancanagus07@gmail.com>
2023-10-09 15:21:11 +0700

    menambahkan routes

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
 	res.send("Hello World!");
 });
 
+//import route posts
+const postsRouter = require('./routes/postsRoutes');
+app.use('/api/posts', postsRouter); // use route posts di Express
+
 app.listen(port, () => {
 	console.log(`app running at http://localhost:${port}`);
 });
