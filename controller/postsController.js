da5629793145d6e383fb83fddd527612a262092e
rozannouval <rozannouval@gmail.com>
2023-10-09 15:25:09 +0700

    mengganti tabel buku menjadi posts

--- controller/postsController.js
+++ controller/postsController.js
@@ -4,17 +4,17 @@ const connection = require("../config/database")
 //function index posts
 const index = (req, res) => {
     //query
-    connection.query('SELECT * FROM buku ORDER BY id desc', 
+    connection.query('SELECT * FROM posts ORDER BY id desc', 
     function (err, rows) {
         if (err) {
             return res.status(500).json({
                 status: false,
-                message: 'Internal Server False',
+                message: 'Kamu Salah',
             })
         } else {
             return res.status(200).json({
                 status: true,
-                message: 'List Data Posts',
+                message: 'Kamu Benar di tabel posts',
                 data: rows
             })
         }
