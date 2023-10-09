5abbff839ee3745b169cbb6e9c2a1f67575c89fa
fhanywikey <fhanywike9@gmail.com>
2023-10-09 15:12:23 +0700

    install nodemon

---
+++ controller/postsController.js
@@ -0,0 +1,21 @@
+//require connection
+const connection = requite("../config/database");
+
+//function index posts
+const index = (req, res) => {
+    //query
+    connection.query('SELECT * FROM posts ORDER BY id desc', function (err, rows) {
+        if (err) {
+            return res.status(500).json({
+                status: false,
+                message: 'Internal Server Error',
+            })
+        } else {
+            return res.status(200).json({
+                status: true,
+                message: 'List Data Posts',
+                data: rows
+            })
+        }
+    });
+};
