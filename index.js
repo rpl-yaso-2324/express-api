e420673fe75755d6015551f026fe8846e9a20ddc
unknown <arinaanggraenitgs@gmail.com>
2023-10-09 15:42:27 +0700

    aku udah ya?

--- index.js
+++ index.js
@@ -2,10 +2,17 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-	res.send("Hello World!");
-});
+const postsRouter = require('./routes/posts');
+app.use('/api/posts', postsRouter);
 
 app.listen(port, () => {
 	console.log(`app running at http://localhost:${port}`);
 });
+
+//import route posts
+const postsRouter = require('./routes/posts');
+
+app.use('/api/posts', postsRouter); // use route posts di Express
+//import route posts
+const postsRouter = require('./routes/posts');
+app.use('/api/posts', postsRouter); // use route posts di Express
---
+++ routes/Controller/postsController.js
@@ -0,0 +1,17 @@
+//import database
+var connection = require('../config/database');
+
+connection.query('SELECT * FROM posts ORDER BY id desc', function (err, rows) {
+	
+	return res.status(500).json({
+        status: false,
+        message: 'Internal Server Error',
+    })
+	
+})
+
+return res.status(200).json({
+    status: true,
+    message: 'List Data Posts',
+    data: rows	
+})
