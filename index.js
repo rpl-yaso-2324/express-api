79ff077b771f7e3e9bae99b0d2ea7081ae4839eb
rozannouval <rozannouval@gmail.com>
2023-10-09 20:33:37 +0700

    codingan dengan respon di dalam html

--- index.js
+++ index.js
@@ -3,13 +3,25 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-	res.send("Kamu berhasil bukan aku ༼ つ ◕_◕ ༽つ <br> LAntas? (⌐■_■)");
+  const responseHTML = `
+        <html>
+            <body>
+			<div>
+                <h1>Kamu berhasil bukan aku ༼ つ ◕_◕ ༽つ <br> LAntas? (⌐■_■)</h1>
+                <a href="/api/posts">
+                    <button>Ke Halaman /api/posts</button>
+                </a>
+				</div>
+            </body>
+        </html>
+    `;
+  res.send(responseHTML);
 });
 
 //import route posts
-const postsRouter = require('./routes/postsRoutes');
-app.use('/api/posts', postsRouter); // use route posts di Express
+const postsRouter = require("./routes/postsRoutes");
+app.use("/api/posts", postsRouter); // use route posts di Express
 
 app.listen(port, () => {
-	console.log(`app running at http://localhost:${port}`);
+  console.log(`app running at http://localhost:${port}`);
 });
