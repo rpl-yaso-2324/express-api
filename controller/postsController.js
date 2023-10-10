96a6aff6b92e430ecbac3e7476e34080b8bae303
andreirwn <irawan090506@gmail.com>
2023-10-10 14:55:03 +0700

    menambah router

---
+++ controller/postsController.js
@@ -0,0 +1,22 @@
+//import database
+const connection = require('../config/database');
+
+const index = (req , res ) => {
+    //query
+    connection.query('SELECT * FROM posts ORDER BY id desc', function (err, rows) {
+        if (err) {
+            return res.status(500).json({
+                status: false,
+                message: 'Internal Server Error',
+            })
+        } else {
+            return res.status(200).json({
+                status: true,
+                message: 'List Data Posts',
+                data: rows
+            })
+        }
+    });
+};
+
+module.exports = {index}; 
