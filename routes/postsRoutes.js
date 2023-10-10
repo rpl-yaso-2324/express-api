aad45592f9f1abb6e2b66def7f39eb63bccf2d30
F18R10 <ramadhanfauzi932@gmail.com>
2023-10-10 08:05:32 +0700

    route index

---
+++ routes/postsRoutes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const{index} = require("../controller/postsController.js");
+
+router.get("/", index);
+
+module.export = router;
