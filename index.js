3630d6c1fb8048ad072fa68a0f1bb261e01049ee
achmadyani17 <ahmadyanigunawan2005>
2023-10-11 13:32:29 +0700

    posts index

--- index.js
+++ index.js
@@ -2,15 +2,24 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-	res.send("Hello World!");
-});
+// app.get("/", (req, res) => {
+// 	res.send("Hai isekai!");
+// });
+
+//import body parser
+const bodyParser = require('body-parser')
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
 
 //import route posts
-const postsRouter = require('./routes/posts');
-app.use('/api/posts', postsRouter); // use route posts di Express
+const postsRouter = require('./routes/routePosts');
+app.use('/api/posts', postingan); // use route posts di Express
 
 app.listen(port, () => {
-  console.log(`app running at http://localhost:${port}`)
+  console.log(`express nya berjalan di jalan ini -> http://localhost:${port}`)
 })
-
