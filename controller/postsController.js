2f5f31c535d78afd01cf3cc0ff59eb18e02065fc
rozannouval <rozannouval@gmail.com>
2023-10-11 13:36:09 +0700

    10/11/2023

--- controller/postsController.js
+++ controller/postsController.js
@@ -1,42 +1,65 @@
+//import express validator
+const { validationResult } = require('express-validator');
 //require connection
 const connection = require("../config/database")
 
 //function index posts
-const index = (req, res) => {
+const index = ('/', function (req, res) {
     //query
     connection.query('SELECT * FROM posts ORDER BY id desc', 
     function (err, rows) {
         if (err) {
             return res.status(500).json({
                 status: false,
-                message: 'Kamu Salah',
+                message: 'gak gitu caranya!!',
             })
         } else {
             return res.status(200).json({
                 status: true,
-                message: 'Kamu Benar di tabel posts',
+                message: 'selamat anda mendapatkan kebenaran di tabel posts',
                 data: rows
             })
         }
     });
-}
+})
 
-const store = (req, res) => {
-    connection.query('SELECT * FROM buku ORDER BY id desc', 
-    function (err, rows) {
+/**
+ * STORE POST
+ */
+function tambahPostingan (req, res) {
+
+    const validationreq = validationResult(req);
+
+    if (!validationreq.isEmpty()) {
+        return res.status(422).json({
+            validationreq: validationreq.array()
+        });
+    }
+
+    //define formData
+    let formData = {
+        title: req.body.title,
+        content: req.body.content,
+    }
+
+    // insert query
+    connection.query('INSERT INTO posts SET ?', formData, function (err, rows) {
+        //if(err) throw err
         if (err) {
             return res.status(500).json({
                 status: false,
-                message: 'Kamu Salah',
+                message: 'Internal Server Error',
             })
         } else {
-            return res.status(200).json({
+            return res.status(201).json({
                 status: true,
-                message: 'Kamu Benar di tabel',
-                data: rows
+                message: 'Insert Data Successfully',
+                data: rows[0]
             })
         }
-    });
-}
+    })
 
-module.exports = { index, store };
+};
+
+
+module.exports = { index, tambahPostingan };
