2fea69eaa6052aa1cff348f0b7a70fd197be9904
Rivaldimaulanaa <maulanarifaldi466@gmail.com>
2023-10-11 12:13:00 +0700

    menambahkan validasi

--- controller/postsController.js
+++ controller/postsController.js
@@ -19,4 +19,42 @@ const index = (req,res) =>{
     });
 };
 
-module.exports = { index };
+/**
+ * STORE POST
+ */
+function tambahpostingan (req, res)  {
+
+    const validationReq = validationResult(req);
+
+    if (!validationReq.isEmpty()) {
+        return res.status(422).json({
+            validationReq: validationReq.array()
+        });
+    }
+
+    //define formData
+    let formData = {
+        title: req.body.title,
+        content: req.body.content
+    }
+
+    // insert query
+    connection.query('INSERT INTO posts SET ?', formData, function (err, rows) {
+        //if(err) throw err
+        if (err) {
+            return res.status(500).json({
+                status: false,
+                message: 'Internal Server Error',
+            })
+        } else {
+            return res.status(201).json({
+                status: true,
+                message: 'Insert Data Successfully',
+                data: rows[0]
+            })
+        }
+    })
+
+};
+
+module.exports = { index, tambahpostingan };
--- routes/postsRouter.js
+++ routes/postsRouter.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const {index} = require("../controller/postsController")
+const { body } = require("express-validator")
 
 router.get("/", index);
+router.post(
+ "/tambahpostingan",
+ [
+    //validator
+    body("title").notEmpty(),
+    body("content").notEmpty(),
+ ],
+ tambahPostingan
+);
 
 module.exports = router;
 
