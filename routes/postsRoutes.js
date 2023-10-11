d06045f9cff0239d83488035781dafded6605d26
rabbanisan <bani.ramadhan99@gmail.com>
2023-10-11 12:08:56 +0700

    merge local git

--- routes/postsRoutes.js
+++ routes/postsRoutes.js
@@ -1,7 +1,7 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const {index} = require("../controller/postsController")
+const { index } = require("../controller/postsController");
 
-router.get ("/",index);
+router.get("/", index);
 
 module.exports = router;
