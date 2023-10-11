4614855e79e9a89fab68ed37b02f37b56a686fd2
cici <cicicisilia4@gmail.com>
2023-10-11 13:29:56 +0700

    posts index

---
+++ routes/routePostingan.js
@@ -0,0 +1,21 @@
+const express  = require('express');
+const router = express.Router();
+const {index} = require ("../controller/controllerPostingan.js");
+
+
+//import express validator
+const { body, validationResult } = require('express-validator');
+
+
+router.get("/", index);
+router.post(
+    "/tambahPostingan",
+[
+//validation
+body('title'),notEmpty(),
+body('content'),notEmpty(),
+],
+tambahPostingan
+);
+
+module.exports =  router;
