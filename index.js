f465317455955feb685e0070185f418efc75a7e0
Rivaldimaulanaa <147567076+Rivaldimaulanaa@users.noreply.github.com>
2023-10-16 14:28:03 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 const { index } = require("../controller/controllerPostingan")
 
-router.get("/", index)
+router.get("/", index) 
 router.post(
     "/tambahPostingan",
     [
