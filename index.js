0c538f2aa7e99ffd2c6f66f20d7f6bfd9946c723
rabbanisan <bani.ramadhan99@gmail.com>
2023-10-16 13:43:14 +0700

    add tambahPostingan

--- index.js
+++ index.js
@@ -2,10 +2,19 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+//import body parser
+const bodyParser = require("body-parser");
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
 //import route posts
-const postsRouter = require("./routes/postsRoutes");
-app.use("/api/posts", postsRouter); // use route posts di Express
+const routePostingan = require("./routes/routePostingan");
+app.use("/api/postingan", routePostingan); // use route posts di Express
 
 app.listen(port, () => {
-	console.log(`app running at http://localhost:${port}`);
+	console.log(`si express berjalan di http://localhost:${port}`);
 });
--- routes/postsRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { index } = require("../controller/postsController");
-
-router.get("/", index);
-
-module.exports = router;
