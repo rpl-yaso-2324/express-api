36476b601cedc0779aa98a16da823e156ab76988
inifadil <rwitular11@gamil.com>
2023-10-16 15:22:44 +0700

    fadhil

---
+++ controller/controllerPostingan.js
@@ -0,0 +1,67 @@
+//require connection
+const connection = require("../config/database");
+
+//import express validator
+const { validationResult } = require("express-validator");
+
+//function index postingan
+function index(req, res) {
+	//query
+	connection.query(
+		"SELECT * FROM posts ORDER BY id desc",
+		function (err, rows) {
+			if (err) {
+				return res.status(500).json({
+					status: false,
+					message: "Internal Server Error",
+				});
+			} else {
+				return res.status(200).json({
+					status: true,
+					message: "List Data Posts",
+					data: rows,
+				});
+			}
+		}
+	);
+}
+
+//function tambah postingan
+function tambahPostingan(req, res) {
+	const validationReq = validationResult(req);
+
+	if (!validationReq.isEmpty()) {
+		return res.status(422).json({
+			errors: validationReq.array(),
+		});
+	}
+
+	//define formData
+	let formData = {
+		title: req.body.title,
+		content: req.body.content,
+	};
+
+	// insert query
+	connection.query(
+		"INSERT INTO postingan SET ?",
+		formData,
+		function (err, rows) {
+			//if(err) throw err
+			if (err) {
+				return res.status(500).json({
+					status: false,
+					message: "Internal Server Error",
+				});
+			} else {
+				return res.status(201).json({
+					status: true,
+					message: "Insert Data Successfully",
+					data: rows[0],
+				});
+			}
+		}
+	);
+}
+
+module.exports = { index, tambahPostingan };
