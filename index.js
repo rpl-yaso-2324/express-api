41b85cd9e19b47791ebc0e030a0608c6a0caa13e
yasri777 <yasrisimbolon47@gmail.com>
2023-10-16 14:10:12 +0700

    error

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-const express = require("express");
-const app = express();
-const port = 3000;
+const express = require('express')
+const app = express()
+const port = 3300
 
-const postsRouter = require('./routes/postsRoutes');
-app.use('/api/posts', postsRouter); 
+//import route posts
+const postsRouter = require('./routes/posts');
+app.use('/api/posts', postsRouter); // use route posts di Express
 
 app.listen(port, () => {
-	console.log(`app running at http://localhost:${port}`);
-});
+  console.log(`app running at http://localhost:${port}`)
+})
