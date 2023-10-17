08f2feb63cc764598ffd5803289fc90b053b4dbe
yanzjj <juanikbalpratama@gmail.com>
2023-10-17 07:41:17 +0700

    update 2

--- index.js
+++ index.js
@@ -2,12 +2,19 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+//import body parser
+const bodyParser = require('body-parser')
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
 
+// parse application/json
+app.use(bodyParser.json())
+
+// import route posts
 const postsRouter = require('./routes/postsRoutes');
 app.use('/api/posts', postsRouter);
 
-
 app.listen(port, () => {
-	console.log(`app running at http://localhost:${port}`);
+    console.log(`berjalan di http://localhost:${port}`);
 });
