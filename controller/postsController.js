1881dbf897c110a19d637b5bcb4b89adde3263fd
yasri777 <yasrisimbolon47@gmail.com>
2023-10-18 08:31:09 +0700

    lanjut benerin nanti

--- controller/postsController.js
+++ controller/postsController.js
@@ -1,20 +1,67 @@
-const connection = require("../config/database")
+//require connection
+const connection = require("../config/database");
 
-const index = (req, res)=> {
-    connection.query('SELECT * FROM posts ORDER BY id desc', function (err, rows) {
-        if (err) {
-            return res.status(500).json({
-                status: false,
-                message: 'Internal Server Error',
-            })
-        } else {
-            return res.status(200).json({
-                status: true,
-                message: 'List Data Posts',
-                data: rows
-            })
-        }
-    });
+//import express validator
+const { validationResult } = require("express-validator");
+
+//function index postingan
+function index(req, res) {
+	//query
+	connection.query(
+		"SELECT * FROM postingan ORDER BY id desc",
+		function (err, rows) {
+			if (err) {
+				return res.status(500).json({
+					status: false,
+					message: "Internal Server Error",
+				});
+			} else {
+				return res.status(200).json({
+					status: true,
+					message: "List Data Posts",
+					data: rows,
+				});
+			}
+		}
+	);
 }
 
-module.exports = {index} ;
+//function tambah postingan
+function tambahPostingan(req, res) {
+	const validationReq = validationResult(req);
+
+	if (!validationReq.isEmpty()) {
+		return res.status(422).json({
+			errors: validationReq.array(),
+		});
+	}
+
+	//define formData
+	let formData = {
+		title: req.body.title,
+		content: req.body.content,
+	};
+
+	// insert query
+	connection.query(
+		"INSERT INTO postingan SET ?",
+		formData,
+		function (err, rows) {
+			//if(err) throw err
+			if (err) {
+				return res.status(500).json({
+					status: false,
+					message: "Internal Server Error",
+				});
+			} else {
+				return res.status(201).json({
+					status: true,
+					message: "Insert Data Successfully",
+					data: rows[0],
+				});
+			}
+		}
+	);
+}
+
+module.exports = { index, tambahPostingan };
--- routes/postsRoutes.js
+++ routes/postsRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const { index, tambahPostingan } = require("../controller/controllerPostingan");
 
 //import express validator
 const { body, validationResult } = require('express-validator');
@@ -11,21 +12,21 @@ const connection = require('../config/database');
  * INDEX POSTS
  */
 router.get('/', function (req, res) {
-    //query
-    connection.query('SELECT * FROM posts ORDER BY id desc', function (err, rows) {
-        if (err) {
-            return res.status(500).json({
-                status: false,
-                message: 'Internal Server Error',
-            })
-        } else {
-            return res.status(200).json({
-                status: true,
-                message: 'List Data Posts',
-                data: rows
-            })
-        }
-    });
+//query
+connection.query('SELECT * FROM posts ORDER BY id desc', function (err, rows) {
+if (err) {
+	return res.status(500).json({
+		status: false,
+		message: 'Internal Server Error',
+	})
+} else {
+	return res.status(200).json({
+status: true,
+message: 'List Data Posts',
+data: rows
+	})
+}
+	});
 });
 
 /**
@@ -33,43 +34,39 @@ router.get('/', function (req, res) {
  */
 router.post('/store', [
 
-    //validation
-    body('title').notEmpty(),
-    body('content').notEmpty()
+	//validation
+	body('title').notEmpty(),
+	body('content').notEmpty()
 
 ], (req, res) => {
 
-    const errors = validationResult(req);
+	const errors = validationResult(req);
 
-    if (!errors.isEmpty()) {
-        return res.status(422).json({
-            errors: errors.array()
-        });
-    }
+	if (!errors.isEmpty()) {
+		return res.status(422).json({
+			errors: errors.array()
+		});
+		}
+	
+		//define formData
+		let formData = {
+			title: req.body.title,
+			content: req.body.content
+		}
 
-    //define formData
-    let formData = {
-        title: req.body.title,
-        content: req.body.content
-    }
-
-    // insert query
-    connection.query('INSERT INTO posts SET ?', formData, function (err, rows) {
-        //if(err) throw err
-        if (err) {
-            return res.status(500).json({
-                status: false,
-                message: 'Internal Server Error',
-            })
-        } else {
-            return res.status(201).json({
-                status: true,
-                message: 'Insert Data Successfully',
-                data: rows[0]
-            })
-        }
-    })
-
-});
+		// insert query
+		connection.query('INSERT INTO posts SET ?', formData, function (err, rows) {
+			
+		}
+router.get("/", index);
+router.post(
+	"/tambahPostingan",
+	[
+		// validation
+		body("title").notEmpty(),
+		body("content").notEmpty(),
+	],
+	tambahPostingan
+);
 
 module.exports = router;
